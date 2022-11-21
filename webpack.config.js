9c61d14b46876caab032d50de6f16edee0c27d76
Paul Sealock <psealock@gmail.com>
2022-11-22 12:52:04 +1300

    prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const defaultConfig = require("@wordpress/scripts/config/webpack.config");
-const WooCommerceDependencyExtractionWebpackPlugin = require("@woocommerce/dependency-extraction-webpack-plugin");
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const WooCommerceDependencyExtractionWebpackPlugin = require( '@woocommerce/dependency-extraction-webpack-plugin' );
 
 module.exports = {
 	...defaultConfig,
 	plugins: [
 		...defaultConfig.plugins.filter(
-			(plugin) =>
-				plugin.constructor.name !== "DependencyExtractionWebpackPlugin"
+			( plugin ) =>
+				plugin.constructor.name !== 'DependencyExtractionWebpackPlugin'
 		),
 		new WooCommerceDependencyExtractionWebpackPlugin(),
 	],

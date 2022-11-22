9f7a3b362d024f161c17ab5059df64fb7c4af22a
Paul Sealock <psealock@gmail.com>
2022-11-22 13:36:55 +1300

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Woo Plugin Setup
+
+A boilerplate for modern WooCommerce development
+
+## Getting Started
+
+### Prerequisites
+
+-   [NPM](https://www.npmjs.com/)
+-   [wp-env](https://developer.wordpress.org/block-editor/reference-guides/packages/packages-env/)
+
+### Installation and Build
+
+```
+npm install
+npm build
+wp-env start
+```
--- woo-plugin-setup.php
+++ woo-plugin-setup.php
@@ -1,9 +1,27 @@
 <?php
 /**
  * Plugin Name: Woo Plugin Setup
+ * Plugin URI: https://github.com/psealock/woo-plugin-setup
+ * Description: This is an example of an extension template to follow.
+ * Version: 1.0.0
+ * Author: WooCommerce
+ * Author URI: https://woocommerce.com
+ * Text Domain: woo-plugin-setup
+ * Domain Path: /languages
+ * Tested up to: 6.1
+ * WC tested up to: 7.1
+ * WC requires at least: 5.8
+ *
+ * Copyright: © 2022 WooCommerce.
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
  *
  * @package WooCommerce\Admin
  */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * Register the JS and CSS.

b05e4a30faa1da9b1154a7c107e7a9ae920058db
Paul Sealock <psealock@gmail.com>
2022-11-23 14:39:58 +1300

    add psr-4

--- woo-plugin-setup.php
+++ woo-plugin-setup.php
@@ -19,68 +19,76 @@
  * @package WooCommerce\Admin
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+defined( 'ABSPATH' ) || exit;
+
+if ( ! defined( 'WOO_PLUGIN_SETUP_PLUGIN_FILE' ) ) {
+	define( 'WOO_PLUGIN_SETUP_PLUGIN_FILE', __FILE__ );
 }
 
-/**
- * Register the JS and CSS.
- */
-function add_extension_register_script() {
-	if ( ! method_exists( 'Automattic\WooCommerce\Admin\PageController', 'is_admin_or_embed_page' ) ||
-		! \Automattic\WooCommerce\Admin\PageController::is_admin_or_embed_page()
-	) {
-		return;
-	}
+require_once __DIR__ . '/vendor/autoload.php';
 
-	$script_path       = '/build/index.js';
-	$script_asset_path = dirname( __FILE__ ) . '/build/index.asset.php';
-	$script_asset      = file_exists( $script_asset_path )
-		? require $script_asset_path
-		: array(
-			'dependencies' => array(),
-			'version'      => filemtime( $script_path ),
-		);
-	$script_url        = plugins_url( $script_path, __FILE__ );
+use WooPluginSetup\Admin\Setup;
 
-	wp_register_script(
-		'{{extension_slug}}',
-		$script_url,
-		$script_asset['dependencies'],
-		$script_asset['version'],
-		true
-	);
+new Setup();
 
-	wp_register_style(
-		'{{extension_slug}}',
-		plugins_url( '/build/index.css', __FILE__ ),
-		// Add any dependencies styles may have, such as wp-components.
-		array(),
-		filemtime( dirname( __FILE__ ) . '/build/index.css' )
-	);
+// /**
+// * Register the JS and CSS.
+// */
+// function add_extension_register_script() {
+// if ( ! method_exists( 'Automattic\WooCommerce\Admin\PageController', 'is_admin_or_embed_page' ) ||
+// ! \Automattic\WooCommerce\Admin\PageController::is_admin_or_embed_page()
+// ) {
+// return;
+// }
 
-	wp_enqueue_script( '{{extension_slug}}' );
-	wp_enqueue_style( '{{extension_slug}}' );
-}
+// $script_path       = '/build/index.js';
+// $script_asset_path = dirname( __FILE__ ) . '/build/index.asset.php';
+// $script_asset      = file_exists( $script_asset_path )
+// ? require $script_asset_path
+// : array(
+// 'dependencies' => array(),
+// 'version'      => filemtime( $script_path ),
+// );
+// $script_url        = plugins_url( $script_path, __FILE__ );
 
-add_action( 'admin_enqueue_scripts', 'add_extension_register_script' );
+// wp_register_script(
+// '{{extension_slug}}',
+// $script_url,
+// $script_asset['dependencies'],
+// $script_asset['version'],
+// true
+// );
 
-/**
- * Register a WooCommerce Admin page.
- */
-function add_extension_register_page() {
-	if ( ! function_exists( 'wc_admin_register_page' ) ) {
-		return;
-	}
+// wp_register_style(
+// '{{extension_slug}}',
+// plugins_url( '/build/index.css', __FILE__ ),
+// Add any dependencies styles may have, such as wp-components.
+// array(),
+// filemtime( dirname( __FILE__ ) . '/build/index.css' )
+// );
 
-	wc_admin_register_page(
-		array(
-			'id'     => 'my-example-page',
-			'title'  => __( 'My Example Page', 'my-textdomain' ),
-			'parent' => 'woocommerce',
-			'path'   => '/example',
-		)
-	);
-}
+// wp_enqueue_script( '{{extension_slug}}' );
+// wp_enqueue_style( '{{extension_slug}}' );
+// }
 
-add_action( 'admin_menu', 'add_extension_register_page' );
+// add_action( 'admin_enqueue_scripts', 'add_extension_register_script' );
+
+// /**
+// * Register a WooCommerce Admin page.
+// */
+// function add_extension_register_page() {
+// if ( ! function_exists( 'wc_admin_register_page' ) ) {
+// return;
+// }
+
+// wc_admin_register_page(
+// array(
+// 'id'     => 'my-example-page',
+// 'title'  => __( 'My Example Page', 'my-textdomain' ),
+// 'parent' => 'woocommerce',
+// 'path'   => '/example',
+// )
+// );
+// }
+
+// add_action( 'admin_menu', 'add_extension_register_page' );

48f38de2b250be6c5c9a0bd37d74cad25e343ebb
Paul Sealock <psealock@gmail.com>
2022-11-28 10:28:18 +1300

    Ad JS i18n

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@
  * External dependencies
  */
 import { addFilter } from '@wordpress/hooks';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import './index.scss';
 
-const MyExamplePage = () => <h1>My Example Extension</h1>;
+const MyExamplePage = () => (
+	<h1>{ __( 'My Example Extension', 'woo-plugin-setup' ) }</h1>
+);
 
 addFilter( 'woocommerce_admin_pages_list', 'my-namespace', ( pages ) => {
 	pages.push( {
 		container: MyExamplePage,
 		path: '/example',
-		breadcrumbs: [ 'My Example Page' ],
+		breadcrumbs: [ __( 'My Example Page', 'woo-plugin-setup' ) ],
 		navArgs: {
 			id: 'my-example-page',
 		},
